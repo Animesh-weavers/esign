c30eca28bd6edb15aed7711f4b805fcf47437c6e
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-09 13:10:16 +0530

    create button and modal popup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import "./App.css";
+import SigModal from "./Component/SigModal";
+
 
 const App = () => {
+  const [showModal,setShowModal]=useState(false);
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <h1>E-Sign</h1>
+      <Button variant="outline-primary" onClick={()=>setShowModal(true)}>Click To Signature</Button>
+      <SigModal  />
+    </div>
+  );
+};
 
-export default App
+export default App;
