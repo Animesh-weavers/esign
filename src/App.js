291ffcf6a4021ab24680588b9870fd693e2b9d4b
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-09 16:43:43 +0530

    complete esign

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import "./App.css";
 import MyVerticallyCenteredModal from "./Component/MyVerticallyCenteredModal";
+import { saveAs } from 'file-saver'
+
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const imageUrlHandler = (imgUrl) => {
+    setImageUrl(imgUrl);
+  };
+
+  const downloaderHandler=()=>{
+    saveAs(imageUrl, 'signature.png')
+  }
+
   return (
-    <div className="App">
-      <h1>E-Sign</h1>
-      <Button variant="outline-primary" onClick={() => setShowModal(true)}>
-        Click To Signature
-      </Button>
-      <MyVerticallyCenteredModal
-        show={showModal}
-        onHide={() => setShowModal(false)}
-      />
-    </div>
+    <>
+      <div className="App">
+        <h1>E-Sign</h1>
+        <Button variant="outline-primary" onClick={() => setShowModal(true)}>
+          Click To Signature
+        </Button>
+        <MyVerticallyCenteredModal
+          show={showModal}
+          onHide={() => setShowModal(false)}
+          imageurlhandler={imageUrlHandler}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "88vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {imageUrl ? (
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={imageUrl} />
+            <Card.Body>
+              <Button variant="primary" onClick={downloaderHandler}>Download</Button>
+            </Card.Body>
+          </Card>
+        ) : (
+          <h2>Kindly Click Blue Button</h2>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/Component/MyVerticallyCenteredModal.js
+++ src/Component/MyVerticallyCenteredModal.js
@@ -1,31 +1,50 @@
-import { Modal,Button } from "react-bootstrap";
+import { useRef } from "react";
+import { Modal, Button } from "react-bootstrap";
+import SignatureCanvas from "react-signature-canvas";
 
-const MyVerticallyCenteredModal=(props) =>{
+const MyVerticallyCenteredModal = (props) => {
+  const sigCanvas = useRef({});
+  const saveHandler = () => {
+    if (sigCanvas.current.isEmpty()) {
+      alert("Signature on black pad!!");
+      return;
+    }
+    console.log("SAVED");
+    props.imageurlhandler(
+      sigCanvas.current.getTrimmedCanvas().toDataURL("image/png")
+    );
+    props.onHide(false);
+  };
+  const clearHandler = () => {
+    console.log("CLEAR");
+    sigCanvas.current.clear();
+  };
   return (
     <Modal
-      {...props}
+      show={props.show}
+      onHide={props.onHide}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Signature
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Signature</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <SignatureCanvas
+          ref={sigCanvas}
+          canvasProps={{ width: 700, height: 200 }}
+          backgroundColor="rgb(0,0,0)"
+          penColor="rgb(0, 221, 255)"
+        />
       </Modal.Body>
       <Modal.Footer>
+        <Button onClick={saveHandler}>Save</Button>
+        <Button onClick={clearHandler}>Clear</Button>
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
-}
+};
 
 export default MyVerticallyCenteredModal;
